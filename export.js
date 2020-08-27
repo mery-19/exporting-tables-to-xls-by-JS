dc6eea23bdec28bab63b49290545b07b600ea70f
mery-19 <60045765+mery-19@users.noreply.github.com>
2020-08-27 17:54:37 +0100

    Create export.js

---
+++ export.js
@@ -0,0 +1,17 @@
+
+function exportTableToExcel(tableID, filename = '')
+{
+    var downloadLink;
+    var dataType = 'application/vnd.ms-excel';
+    var tableSelect = document.getElementById(tableID);
+    var tableHTML = tableSelect.outerHTML.replace(/ /g, '%20');
+   
+    filename = filename?filename+'.xls':'excel_data.xls';
+    
+    downloadLink = document.createElement("a");
+    
+    document.body.appendChild(downloadLink);
+    downloadLink.href = 'data:' + dataType + ', ' + tableHTML;
+    downloadLink.download = filename;
+    downloadLink.click();
+}
